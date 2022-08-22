339984eaf70fe32ede29dd77990a086806f38dc9
Artur Monteiro <Artur@scopi.com.br>
2022-08-21 22:37:55 -0300

    fix(prisma): removendo migrations de teste

--- prisma/migrations/20220821220706_steps/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Steps" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "recipeId" INTEGER NOT NULL,
-    "description" TEXT NOT NULL,
-    CONSTRAINT "Steps_recipeId_fkey" FOREIGN KEY ("recipeId") REFERENCES "Recipe" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
