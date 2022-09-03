18ef512edf985fa759e9202ccd807c3a5b483f0b
Artur Monteiro <Artur@scopi.com.br>
2022-09-03 20:12:10 -0300

    fix(Users): mostrando mensagens de acordo para cadastro

--- pages/admin/Users.tsx
+++ pages/admin/Users.tsx
@@ -8,7 +8,12 @@ const Users: NextPage = () => {
   const [email, setEmail] = React.useState('');
   const [name, setName] = React.useState('');
 
-  const signinMutate = trpc.useMutation(['user.createUser']);
+  const signinMutate = trpc.useMutation(['user.createUser'], {
+    onSuccess: () => {
+      setEmail('');
+      setName('');
+    }
+  });
   const res = trpc.useQuery(['user.users']);
 
   const handleSubmitSignin: React.FormEventHandler<HTMLFormElement> = (e: React.FormEvent<HTMLFormElement>) => {
@@ -26,6 +31,9 @@ const Users: NextPage = () => {
         <TextField id='email-input' label='Email' variant='outlined' onChange={(e) => setEmail(e.target.value)}/>
         <Button type='submit' variant="contained"><span>Criar</span></Button>
       </form>
+
+      {signinMutate.isLoading && <p>Carregando...</p>}
+      {signinMutate.isSuccess && <p>Usuário criado com sucesso!</p>}
 
       {res?.data && res?.data.length > 0 && (
         <>
