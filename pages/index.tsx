09d409784ecb3c2c9e49e3597df5d8ebb8d244f2
Artur Monteiro <Artur@scopi.com.br>
2022-09-04 22:29:26 -0300

    fix(Index): estilizacao de titulo

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Login from '../components/login';
 const Home: NextPage = () => {
   return (
     <div className="start-page">
-      <Typography className="title">
+      <Typography variant="h1" className="title">
         Receiquick
       </Typography>
       <Login/>
