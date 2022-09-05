413c072eaa31f25cb15e6e02853632c482c4198b
Artur Monteiro <Artur@scopi.com.br>
2022-09-04 22:24:06 -0300

    fix(Index): adicionando titulo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
 import Login from '../components/login';
 
 const Home: NextPage = () => {
   return (
     <div className="start-page">
+      <Typography className="title">
+        Receiquick
+      </Typography>
       <Login/>
     </div>
   )
